d835b859b96231c18a738d4f92b8fc4cccaad0d8
Jeff Boes <jeff@endpoint.com>
2010-12-17 14:22:26 +0000

    Add ignorable support, config add_class

--- share/ic/js/renderer/form_wrapper.js
+++ share/ic/js/renderer/form_wrapper.js
@@ -18,23 +18,33 @@
 YUI.add(
     "ic-renderer-form_wrapper",
     function(Y) {
+        var _plugin_name_map = {
+            ignorable: Y.IC.Plugin.Ignorable
+        };
+
         var Clazz = Y.namespace("IC").RendererFormWrapper = Y.Base.create(
             "ic_renderer_form_wrapper",
             Y.IC.RendererBase,
             [],
             {
                 _form: null,
+                _config: null,
 
                 initializer: function (config) {
                     Y.log(Clazz.NAME + "::initializer");
-                    //Y.log(Clazz.NAME + "::initializer - config: " + Y.dump(config));
+                    Y.log(Clazz.NAME + "::initializer - config: " + Y.dump(config));
+                    this._config = config;
 
                     if (! Y.Lang.isValue(config.form_kind)) {
                         config.form_kind = "simple"
                     }
 
                     this._form = new Y.IC.Form (config.form_config);
-                    //Y.log(Clazz.NAME + "::initializer - _form: " + this._form);
+                    if (Y.Lang.isValue(config.add_class)) {
+                        this.get("contentBox").addClass(config.add_class);
+                    }
+
+                    Y.log(Clazz.NAME + "::initializer - _form: " + this._form);
                 },
 
                 destructor: function () {
@@ -45,10 +55,23 @@ YUI.add(
 
                 renderUI: function () {
                     Y.log(Clazz.NAME + "::renderUI");
-                    //Y.log(Clazz.NAME + "::renderUI - contentBox: " + this.get("contentBox"));
+                    Y.log(Clazz.NAME + "::renderUI - contentBox: " + this.get("contentBox"));
 
                     if (this.get("caption") !== "") {
                         this.get("contentBox").setContent('<span class="ic_renderer_form_caption">' + this.get("caption") + '</span>');
+                    }
+
+                    if (Y.Lang.isValue(this._config.plugins)) {
+                        Y.log(Clazz.NAME + "::renderUI has plugins - " + Y.dump(this._config.plugins));
+                        Y.each(
+                               this._config.plugins,
+                               function (plugin_item) {
+                                   var plugin = _plugin_name_map[plugin_item];
+                                   Y.log(Clazz.NAME + "::renderUI - plugging " + plugin_item);
+                                   this._form.plug(plugin);
+                               },
+                               this
+                        );
                     }
 
                     this._form.render(this.get("contentBox"));
@@ -68,7 +91,8 @@ YUI.add(
         requires: [
             "ic-renderer-form_wrapper-css",
             "ic-renderer-base",
-            "ic-form"
+            "ic-form",
+            "ic-plugin-ignorable"
         ]
     }
 );
